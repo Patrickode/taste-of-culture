f701f919ccf19bf783cce1bf12eedff0a68fe409
crfair <cfair.5298@gmail.com>
2021-11-02 13:31:30 -0400

    added support for changing the mentor's expression

--- A Taste of Culture/Assets/Scripts/DialogueManager.cs
+++ A Taste of Culture/Assets/Scripts/DialogueManager.cs
@@ -9,26 +9,36 @@ public class DialogueManager : MonoBehaviour
     public Text dialogueText;
 
     public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
     private Queue<string> sentences;
+    private Queue<Sprite> expressions;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Start");
         sentences = new Queue<string>();
+        expressions = new Queue<Sprite>();
     }
 
     public void StartDialogue(Dialogue dialogue)
     {
+        Debug.Log("StartDialogue");
         animator.SetBool("IsOpen", true);
 
         nameText.text = dialogue.name;
 
         sentences.Clear();
+        expressions.Clear();
 
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
+        }
+        foreach (Sprite expression in dialogue.expressions)
+        {
+            expressions.Enqueue(expression);
         }
 
         DisplayNextSentence();
@@ -36,6 +46,7 @@ public class DialogueManager : MonoBehaviour
 
     public void DisplayNextSentence()
     {
+        Debug.Log("DisplayNextSentence");
         if (sentences.Count == 0)
         {
             EndDialogue();
@@ -43,13 +54,16 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
+        Sprite expression = expressions.Dequeue();
+        spriteRenderer.sprite = expression;
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
-    
+
     }
 
     IEnumerator TypeSentence(string sentence)
     {
+        Debug.Log("TypeSentence");
         dialogueText.text = "";
         foreach (char letter in sentence.ToCharArray())
         {
@@ -60,6 +74,7 @@ public class DialogueManager : MonoBehaviour
 
     void EndDialogue()
     {
+        Debug.Log("EndDialogue");
         animator.SetBool("IsOpen", false);
     }
 }
