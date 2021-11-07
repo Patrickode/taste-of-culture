4adb893c7b85ba8ffad3b35497ddb65be2669f92
crfair <cfair.5298@gmail.com>
2021-11-07 17:14:12 -0500

    i recognize that i could do this even easier if i used inheritance but i don't have it in me to relearn that rn

---
+++ A Taste of Culture/Assets/Scripts/DialogueManagers/FullscreenDialogueManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// This handles the initialization and ending of the dialogue, printing it to screen, etc.
+public class FullscreenDialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public DialogueSceneManager sceneManager;
+
+    private Queue<string> sentences;
+    private Queue<Sprite> expressions;
+
+    private GameObject continueButton;
+
+    // Initializations. Make sure all the text fields and mentor are not visible yet.
+    void Start()
+    {
+        sentences = new Queue<string>();
+        expressions = new Queue<Sprite>();
+        continueButton = GameObject.Find("ContinueButton");
+
+        nameText.enabled = false;
+        dialogueText.enabled = false;
+        continueButton.SetActive(false);
+        spriteRenderer.enabled = false;
+
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+
+        // Fade in dialogue box and make the text fields visible
+        animator.SetBool("IsOpen", true);
+
+        nameText.enabled = true;
+        dialogueText.enabled = true;
+        continueButton.SetActive(true);
+        spriteRenderer.enabled = true;
+
+        nameText.text = dialogue.name;
+
+        // Clear anything that may be lingering in the queues
+        sentences.Clear();
+        expressions.Clear();
+
+        // Add dialogue and corresponding expressions to the queues
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        foreach (Sprite expression in dialogue.expressions)
+        {
+            expressions.Enqueue(expression);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        // Pop out sentence and expression and type them on screen
+        string sentence = sentences.Dequeue();
+        Sprite expression = expressions.Dequeue();
+        spriteRenderer.sprite = expression;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        // For each letter, type it on screen and wait for a very short period of time
+        // This creates the scrolling effect
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
+
+    void EndDialogue()
+    {
+        // Hide everything and fade out the dialogue box
+        continueButton.SetActive(false);
+        nameText.enabled = false;
+        dialogueText.enabled = false;
+        spriteRenderer.enabled = false;
+        animator.SetBool("IsOpen", false);
+
+        if (sceneManager != null && sceneManager.dialogueString == "intro")
+        {
+            sceneManager.IntroEnded();
+        }
+        else if (sceneManager != null && sceneManager.dialogueString == "error")
+        {
+            sceneManager.MarginsEnded();
+        }
+    }
+}
--- A Taste of Culture/Assets/Scripts/SceneManagers/DialogueSceneManager.cs
+++ A Taste of Culture/Assets/Scripts/SceneManagers/DialogueSceneManager.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueSceneManager : MonoBehaviour
 {
+    public GameObject knife;
+    public CookingDialogueManager dialogueManager;
+    public CookingDialogueTrigger dialogueTrigger;
+    public GameObject dialogue;
+    public string dialogueString;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogue.SetActive(true);
+        knife.SetActive(false);
+        dialogueString = "intro";
+        StartCoroutine(TriggerIntro());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TriggerIntro()
     {
-        
+        yield return new WaitForSeconds(0.5f);
+        dialogueTrigger.TriggerDialogue();
+    }
+
+    public void IntroEnded()
+    {
+        dialogue.SetActive(false);
+        knife.SetActive(true);
+        Cursor.visible = false;
+    }
+
+    public void CutOutsideMargins()
+    {
+        knife.SetActive(false);
+        Cursor.visible = true;
+        dialogue.SetActive(true);
+        dialogueString = "error";
+        dialogueTrigger.TriggerDialogue();
+    }
+
+    public void MarginsEnded()
+    {
+        dialogue.SetActive(false);
+        knife.SetActive(true);
+        Cursor.visible = false;
+    }
+
+    public void FinishedCutting()
+    {
+
     }
 }
