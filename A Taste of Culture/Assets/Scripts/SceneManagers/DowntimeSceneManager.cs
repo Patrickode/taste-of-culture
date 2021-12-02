019ce3b65dfde1709ce56fedb9807f330652f633
crfair <cfair.5298@gmail.com>
2021-12-02 13:50:23 -0500

    minor dialogue and timing tweaks

--- A Taste of Culture/Assets/Scripts/SceneManagers/DowntimeSceneManager.cs
+++ A Taste of Culture/Assets/Scripts/SceneManagers/DowntimeSceneManager.cs
@@ -92,7 +92,7 @@ public class DowntimeSceneManager : MonoBehaviour
     {
         mentor.SetActive(false);
         backgrounds[10].SetActive(true);
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(3.0f);
         backgrounds[11].SetActive(true);
         mentor.SetActive(true);
     }
@@ -111,7 +111,7 @@ public class DowntimeSceneManager : MonoBehaviour
 
     IEnumerator BackgroundToSchool()
     {
-        yield return new WaitForSeconds(4.0f);
+        yield return new WaitForSeconds(3.5f);
         backgrounds[11].SetActive(true);
         mentor.SetActive(true);
     }
