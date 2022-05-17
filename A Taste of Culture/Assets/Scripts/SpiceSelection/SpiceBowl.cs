80765cbb5a7e1c4d7594194f6cb8f5cd2c3f3d1c
Patrick <prm5983@rit.edu>
2022-05-17 17:44:27 -0400

    New conditions for spice bowl tooltip activation
    These new conditions ensure that the dialogue box does not disappear when hovering over at least one bowl, while still updating to show the most recent spice bowl collision

--- A Taste of Culture/Assets/Scripts/SpiceSelection/SpiceBowl.cs
+++ A Taste of Culture/Assets/Scripts/SpiceSelection/SpiceBowl.cs
@@ -5,52 +5,101 @@ using UnityEngine;
 public class SpiceBowl : MonoBehaviour
 {
     enum TypeOfSpice { CayennePepper, Cumin, Ginger, Garlic, Paprika, Cinnamon, Nutmeg, Coriander, Salt };
-    
+
     [SerializeField] TypeOfSpice spiceCategory;
     [SerializeField] GameObject pinchedSpicePrefab;
 
     public CookingDialogueTrigger dialogueTrigger;
     // public GameObject dialogue;
-    
+
     HandController hand;
     SpiceStation spiceStation;
 
-    bool colliding;
+    /// <summary>
+    /// Which SpiceBowl owns the current active tooltip? (null if there is no active tooltip)<br/>
+    /// </summary>
+    private SpiceBowl activeTooltipOwner;
+    private static System.Action<SpiceBowl> TooltipOwnerStateChange;
+    private void OnSpiceTooltipStateChange(SpiceBowl owner) => activeTooltipOwner = owner;
 
-    void Awake() 
+    /// <summary>
+    /// How many bowls the hand is hovering over right now.
+    /// </summary>
+    private int spiceBowlsHovered;
+    private static System.Action<bool> TriggerOccupiedStateChange;
+    private void OnTriggerOccupiedStateChange(bool occupied) => spiceBowlsHovered += occupied ? 1 : -1;
+
+    private Coroutine DisableTooltipCorout;
+
+    void Awake()
     {
         hand = FindObjectOfType<HandController>();
 
         spiceStation = FindObjectOfType<SpiceStation>();
+
+        TooltipOwnerStateChange += OnSpiceTooltipStateChange;
+        TriggerOccupiedStateChange += OnTriggerOccupiedStateChange;
+    }
+    private void OnDestroy()
+    {
+        TooltipOwnerStateChange -= OnSpiceTooltipStateChange;
+        TriggerOccupiedStateChange -= OnTriggerOccupiedStateChange;
     }
 
-    void Update() 
+    void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) &&
+            activeTooltipOwner && activeTooltipOwner == this &&
+            spiceStation.CanDisplayTooltip)
         {
-            if(colliding && spiceStation.CanDisplayTooltip) { hand.SpicePrefab = pinchedSpicePrefab; }
+            hand.SpicePrefab = pinchedSpicePrefab;
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other) 
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag != "Hand") { return; }
+        if (!other.gameObject.CompareTag("Hand")) { return; }
 
-        if(spiceStation.CanDisplayTooltip) 
+        TriggerOccupiedStateChange?.Invoke(true);
+        TryEnableTooltip();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Hand")) { return; }
+
+        if (!activeTooltipOwner)
+            TryEnableTooltip();
+    }
+
+    void TryEnableTooltip()
+    {
+        if (spiceStation.CanDisplayTooltip)
         {
+            TooltipOwnerStateChange?.Invoke(this);
+            Coroutilities.TryStopCoroutine(this, ref DisableTooltipCorout);
+
             dialogueTrigger.TriggerDialogue();
-            colliding = true;
         }
     }
 
-    void OnTriggerExit2D(Collider2D other) 
+    void OnTriggerExit2D(Collider2D other)
     {
-        if(other.gameObject.tag != "Hand") { return; }
+        if (!other.gameObject.CompareTag("Hand")) { return; }
 
-        if(spiceStation.CanDisplayTooltip) 
+        TriggerOccupiedStateChange?.Invoke(false);
+        if (activeTooltipOwner == this)
+            TooltipOwnerStateChange?.Invoke(null);
+
+        DisableTooltipCorout = Coroutilities.DoAfterDelayFrames(this, TryDisableTooltip, 1);
+    }
+
+    void TryDisableTooltip()
+    {
+        if (spiceStation.CanDisplayTooltip && spiceBowlsHovered < 1)
         {
+            Debug.Log($"Disabling tooltips; {name} was the last occupant and just exited");
             dialogueTrigger.DisableDialogue();
-            colliding = false;
         }
     }
 }
