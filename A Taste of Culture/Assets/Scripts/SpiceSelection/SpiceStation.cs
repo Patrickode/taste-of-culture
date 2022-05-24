60d2858f3323d99eca1929a184663f1ac5842093
Patrick <prm5983@rit.edu>
2022-05-24 14:59:11 -0400

    Made reset/done buttons disable during scolding
    Also added two new lines for repeat offenders

--- A Taste of Culture/Assets/Scripts/SpiceSelection/SpiceStation.cs
+++ A Taste of Culture/Assets/Scripts/SpiceSelection/SpiceStation.cs
@@ -7,21 +7,42 @@ public class SpiceStation : MonoBehaviour
     public CookingDialogueTrigger dialogueTrigger;
     public float dialogueTimer = 2.5f;
 
+    [SerializeField] private UnityEngine.UI.Button[] buttonsToDisable;
+    private int timesScolded = 0;
+
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (!(other.gameObject.tag == "Spice")) { return; }
+        if (!other.gameObject.CompareTag("Spice"))
+            return;
 
         StartCoroutine(ScoldPlayer());
     }
 
     IEnumerator ScoldPlayer()
     {
-        dialogueTrigger.TriggerDialogue();
-        SpiceBowl.CanDisplayTooltip = false;                      // Prevent new tooltip from being displayed while player is being scolded.
+        if (timesScolded > 0)
+        {
+            dialogueTrigger.dialogue.sentences[0] = timesScolded < 2
+                ? "Oh, don't worry, I'm used to mess-makers."
+                : "...";
+        }
 
+        dialogueTrigger.TriggerDialogue();
+        SpiceBowl.CanDisplayTooltip = false;    // Prevent new tooltip from being displayed while player is being scolded.
+        TryToggleButtons(false);
+
+        timesScolded++;
         yield return new WaitForSeconds(2.5f);
 
         dialogueTrigger.DisableDialogue();
         SpiceBowl.CanDisplayTooltip = true;
+        TryToggleButtons(true);
+    }
+
+    private void TryToggleButtons(bool enabled)
+    {
+        foreach (var button in buttonsToDisable)
+            if (button)
+                button.interactable = enabled;
     }
 }
