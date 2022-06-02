1f1428ee4e932063317438366870781ba3c955cf
Patrick <prm5983@rit.edu>
2022-06-02 14:08:50 -0400

    Made it so you need to hold down lmb to stir

---
+++ A Taste of Culture/Assets/Scripts/LowerSpoon.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowerSpoon : MonoBehaviour
+{
+    [SerializeField] private Transform visuals;
+    [SerializeField] private GameObject shadow;
+    [SerializeField] private Collider2D spoonCollider;
+    [SerializeField] private Vector3 raisedOffset;
+    private Vector3 originalPos;
+
+    private void Start()
+    {
+        originalPos = visuals.localPosition;
+        ToggleLower(false);
+    }
+
+    private void Update()
+    {
+        ToggleLower(Input.GetKey(KeyCode.Mouse0));
+    }
+
+    private void ToggleLower(bool lowered)
+    {
+        visuals.localPosition = originalPos + (lowered ? Vector3.zero : raisedOffset);
+        shadow.SetActive(!lowered);
+        spoonCollider.enabled = lowered;
+    }
+}
