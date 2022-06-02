abf649c17ef4176f91bbd8a1f71b5bb12accc741
Patrick <prm5983@rit.edu>
2022-06-02 13:49:45 -0400

    Added clamping of onion angular velocity

--- A Taste of Culture/Assets/Scripts/ClampVelocity.cs
+++ A Taste of Culture/Assets/Scripts/ClampVelocity.cs
@@ -7,7 +7,10 @@ public class ClampVelocity : MonoBehaviour
     [SerializeField] private Rigidbody rb3D;
     [SerializeField] private Rigidbody2D rb2D;
     [Space(5)]
+    [SerializeField] private bool clampLinear = true;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private bool clampAngular;
+    [SerializeField] private float maxAngSpeed;
 
     private void FixedUpdate()
     {
@@ -17,17 +20,50 @@ public class ClampVelocity : MonoBehaviour
             enabled = false;
         }
 
-        Vector3 velToClamp = rb3D ? rb3D.velocity : (Vector3)rb2D.velocity;
-        float speed = velToClamp.magnitude;
-
-        if (speed > maxSpeed)
+        if (clampLinear)
         {
-            Vector3 brakingForce = -velToClamp.normalized * (speed - maxSpeed);
-
             if (rb3D)
-                rb3D.AddForce(brakingForce, ForceMode.Impulse);
+                rb3D.AddForce(
+                    GetVectorBrakingForce(rb3D.velocity, maxSpeed),
+                    ForceMode.Impulse);
             else
-                rb2D.AddForce(brakingForce, ForceMode2D.Impulse);
+                rb2D.AddForce(
+                    GetVectorBrakingForce(rb2D.velocity, maxSpeed),
+                    ForceMode2D.Impulse);
         }
+
+        if (clampAngular)
+        {
+            if (rb3D)
+                rb3D.AddTorque(
+                    GetVectorBrakingForce(rb3D.angularVelocity, maxAngSpeed),
+                    ForceMode.Impulse);
+            else
+                //We need inertia because, apparently, AddTorque operates in radians, despite the fact angVel
+                //is measured in degrees; see https://docs.unity3d.com/ScriptReference/Rigidbody2D.AddTorque.html
+                rb2D.AddTorque(
+                    GetFloatBrakingForce(rb2D.angularVelocity, maxAngSpeed, rb2D.inertia),
+                    ForceMode2D.Impulse);
+        }
+    }
+
+    private Vector3 GetVectorBrakingForce(Vector3 forceToClamp, float maxMagnitude)
+    {
+        float speed = forceToClamp.magnitude;
+        if (speed > maxMagnitude)
+            return -forceToClamp.normalized * (speed - maxSpeed);
+
+        return Vector3.zero;
+    }
+
+    private float GetFloatBrakingForce(float forceToClamp, float maxMagnitude, float inertia)
+    {
+        if (forceToClamp > maxMagnitude || forceToClamp < -maxMagnitude)
+        {
+            //Invert the signed distance between the clamped & max; this works regardless of > or <
+            return (-1 * (forceToClamp - Mathf.Sign(forceToClamp) * maxMagnitude) * Mathf.Deg2Rad) * inertia;
+        }
+
+        return 0;
     }
 }
