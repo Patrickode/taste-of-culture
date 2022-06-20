6b903f3ef237ea0e1565235af2e8f5b242d529c9
crfair <cfair.5298@gmail.com>
2022-06-20 00:13:24 -0400

    going to start messing with the plugin code

--- A Taste of Culture/Assets/Scripts/SceneManagers/DialogueController.cs
+++ A Taste of Culture/Assets/Scripts/SceneManagers/DialogueController.cs
@@ -1,10 +1,17 @@
 using DialogueEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DialogueController : MonoBehaviour
 {
     [SerializeField]
     private NPCConversation initConversation;
+    [SerializeField]
+    private string nextScene;
+
+    private Protein protein;
+
+    public enum Protein { chicken, tofu };
 
     void Start()
     {
@@ -14,5 +21,29 @@ public class DialogueController : MonoBehaviour
     private void TriggerConversation(NPCConversation conversation)
     {
         ConversationManager.Instance.StartConversation(conversation);
+        ConversationManager.OnConversationEnded += ConversationEnd;
+    }
+
+    public Protein getProtein()
+    {
+        return protein;
+    }
+
+    public void setProtein(int numCode)
+    {
+        switch (numCode)
+        {
+            case 0:
+                protein = Protein.chicken;
+                break;
+            case 1:
+                protein = Protein.tofu;
+                break;
+        }
+    }
+
+    private void ConversationEnd()
+    {
+        SceneManager.LoadScene(nextScene);
     }
 }
