32c52decabc83edb991d2a3cd232cd728645f628
Patrick <prm5983@rit.edu>
2022-06-21 14:37:29 -0400

    Duplicate onion sauteing and start tweaks

--- A Taste of Culture/Assets/Scripts/LowerSpoon.cs
+++ A Taste of Culture/Assets/Scripts/LowerSpoon.cs
@@ -7,10 +7,11 @@ public class LowerSpoon : MonoBehaviour
     [SerializeField] private Transform visuals;
     [SerializeField] private GameObject shadow;
     [SerializeField] private Collider2D spoonCollider;
+    [SerializeField] private AudioPlayer audioPlayer;
     [Space(5)]
     [SerializeField] private Vector3 raisedOffset;
+    
     private Vector3 originalPos;
-    [SerializeField] private AudioPlayer audioPlayer;
     private bool wasRaised;
     private bool isRaised;
 
@@ -24,7 +25,7 @@ public class LowerSpoon : MonoBehaviour
     private void Update()
     {
         ToggleLower(Input.GetKey(KeyCode.Mouse0));
-        if (wasRaised && !isRaised)
+        if (audioPlayer && wasRaised && !isRaised)
         {
             audioPlayer.TriggerSFX();
         }
