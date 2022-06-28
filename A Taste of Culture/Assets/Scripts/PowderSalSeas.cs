c157bc7aa82ff50c2365f76a712b5ff0aff576c9
Patrick <prm5983@rit.edu>
2022-06-28 13:33:46 -0400

    Switch liquid vs powder mix action (befits each better)

--- A Taste of Culture/Assets/Scripts/PowderSalSeas.cs
+++ A Taste of Culture/Assets/Scripts/PowderSalSeas.cs
@@ -5,23 +5,18 @@ using UnityEngine;
 public class PowderSalSeas : SaladSeasoning
 {
     [Header("Powder Seasoning Fields")]
+    [Tooltip("The minimum scale of this object will be localScale * minimumScaleFactor.")]
     [SerializeField] [Range(0, 1)] private float minimumScaleFactor;
     private Vector3 originalScale;
-    private float originalAlpha;
 
     protected override void Start()
     {
         base.Start();
         originalScale = transform.localScale;
-        originalAlpha = spRend.color.a;
     }
 
     protected override void MixAction()
     {
-        Color newColor = spRend.color;
-        newColor.a = Mathf.Lerp(originalAlpha, 0, MixProgress);
-        spRend.color = newColor;
-
         transform.localScale = Vector3.Lerp(originalScale, originalScale * minimumScaleFactor, MixProgress);
     }
 }
