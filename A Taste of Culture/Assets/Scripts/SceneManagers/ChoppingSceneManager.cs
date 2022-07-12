90edf6fe74dbe6b785758b1a19f2ebf7de5a3dc3
Patrick <prm5983@rit.edu>
2022-07-12 10:56:57 -0400

    Remove requirement for specific hierarchy structure

--- A Taste of Culture/Assets/Scripts/SceneManagers/ChoppingSceneManager.cs
+++ A Taste of Culture/Assets/Scripts/SceneManagers/ChoppingSceneManager.cs
@@ -7,37 +7,37 @@ public class ChoppingSceneManager : BaseIngredientSceneManager
     [SerializeField] List<GameObject> ingredients;
     [SerializeField] List<GameObject> instructions;
     [Space(5)]
-    
-    [SerializeField] GameObject progressBar;
+    [SerializeField] GameObject progressFill;
+    Vector3 progressScale = Vector3.up;
 
     GameObject currentIngredient;
     GameObject currentInstruction;
 
-    GameObject progressIndicator;
-
     public bool bAtLastIngredient = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        if(!(ingredients.Count == 0)) { currentIngredient = ingredients[0]; }
-        else { Debug.Log("Ingredient list is empty!"); }
+        if (!(ingredients.Count == 0))
+            currentIngredient = ingredients[0];
+        else Debug.Log("Ingredient list is empty!");
 
-        if(!(instructions.Count == 0)) { currentInstruction = instructions[0]; }
-        else { Debug.Log("Instruction list is empty!"); }
+        if (!(instructions.Count == 0))
+            currentInstruction = instructions[0];
+        else Debug.Log("Instruction list is empty!");
 
-        progressIndicator = progressBar.transform.GetChild(1).transform.Find("Foreground").gameObject;
-        if(progressIndicator != null) { progressIndicator.transform.localScale = new Vector2(0, 1); }
+        if (progressFill != null)
+            progressFill.transform.localScale = progressScale;
     }
 
     protected override IEnumerator CompleteTask()
     {
         base.CompleteTask();
 
-        if(progressIndicator != null) 
-        { 
-            float progress = (float)(ingredients.IndexOf(currentIngredient) + 1) / (float)ingredients.Count;
-            progressIndicator.transform.localScale = new Vector2(progress, 1);
+        if (progressFill != null)
+        {
+            progressScale.x = (ingredients.IndexOf(currentIngredient) + 1f) / ingredients.Count;
+            progressFill.transform.localScale = progressScale;
         }
 
         // If the current ingredient isn't the last in the list, disable it and enable the next ingredient
@@ -47,7 +47,7 @@ public class ChoppingSceneManager : BaseIngredientSceneManager
 
             int currentIngredientIndex = ingredients.IndexOf(currentIngredient);
             int currentInstructionIndex = instructions.IndexOf(currentInstruction);
-            
+
             if (currentIngredient != null) { currentIngredient.SetActive(false); }
             if (currentInstruction != null) { currentInstruction.SetActive(false); }
 
@@ -55,7 +55,7 @@ public class ChoppingSceneManager : BaseIngredientSceneManager
             if (spriteMask != null) { GameObject.Destroy(spriteMask); }
 
             currentIngredient = ingredients[currentIngredientIndex + 1];
-            if(ingredients.IndexOf(currentIngredient) == ingredients.Count - 1) { bAtLastIngredient = true; }
+            if (ingredients.IndexOf(currentIngredient) == ingredients.Count - 1) { bAtLastIngredient = true; }
 
             currentInstruction = instructions[currentInstructionIndex + 1];
 
